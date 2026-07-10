9537f3fe200dfdf363222585f5bdd50e0b3e4529
Adema <zloisviter@gmail.com>
2026-07-11 00:34:46 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 400,
+        height: 600,
+        resizable: false
+    })
+
+    win.loadFile('index.html')
+
+    win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+  }
+})
